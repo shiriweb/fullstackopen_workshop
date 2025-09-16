33ef8d3580268f8802c5955b0030275448b812fe
shiriweb <shirisha.singh36@gmail.com>
2025-09-16 11:43:28 +0545

    feat: add controlled component concept

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./components/Note";
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNotes, SetNewNotes] = useState("Type something...");
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target);
+
+    const object = {
+      id: notes[notes.length - 1].id + 1,
+      content: newNotes,
+      important: Math.random() < 0.5,
+    };
+
+    setNotes([...notes, object]);
+    SetNewNotes("");
+  }
+
+  function handleChange(event) {
+    SetNewNotes(event.target.value);
+  }
+
   return (
     <div>
       <h1>My Notes</h1>
@@ -9,6 +29,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={newNotes} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
