13044a5114c3135e0ff87445167bace6e8d00bea
shiriweb <shirisha.singh36@gmail.com>
2025-10-10 12:41:05 +0545

    feat: fetching objects from the database

--- part3/notes-server/mongo.js
+++ part3/notes-server/mongo.js
@@ -1,27 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log("give password as argument");
+  process.exit(1);
 }
 
 ////////////
 // Connecting database
-const password = process.argv[2]
-const url = `mongodb+srv://shirisha:${password}@cluster0.q8zc9wj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const password = process.argv[2];
+const url = `mongodb+srv://shirisha:${password}@cluster0.q8zc9wj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.connect(url);
 //////////////
 
 // ----------------
-// defining the schema 
+// defining the schema
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 // ------------------
 
-
 // creating the model for the schema
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
+// Instance of the model
+// Creating the new the data
+
+// const note = new Note({
+//   content: "Js is interesting",
+//   important: false,
+// });
+
+// Saving the data to the database
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+// fetching the data from the database
+// Note.findById('68e89605f8d4a55b16eb1423').then((result) => {
+// //   result.forEach((note) => {
+//     console.log(result);
+// //   });
+//   mongoose.connection.close();
+// });
+
+
+// using async/await 
+async function getBYId() {
+  const result = await Note.findById("68e8a519a0910d06ffa70d49");
+  console.log(result);
+}
+getBYId();
