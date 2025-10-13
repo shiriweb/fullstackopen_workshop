b43e226279d194b188140c4107268983487f4058
shiriweb <shirisha.singh36@gmail.com>
2025-10-13 11:35:52 +0545

    fead: concept of error handling

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -42,50 +42,96 @@ let notes = [];
 // })
 
 // fetching all the notes
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((result) => {
-    response.status(200).send(result);
-  });
+app.get("/api/notes", (request, response, next) => {
+  Note.find({})
+    .then((result) => {
+      response.status(200).send(result);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
   const note = new Note({
     content: body.content,
-    important: false,
+    important: body.important || false,
   });
 
-  // Saving the data to the database
-  note.save().then((result) => {
-    console.log("note saved!");
-    response.status(201).send(result);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-
 // fetching the note by id
-app.get("/api/notes/:id", (request, response) => {
-  const myId = request.params.id;
-  const myNote = notes.find((note) => note.id === myId);
-  if (myNote) {
-    response.json(myNote);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      response.json(note);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 // Deleting the specific note
-app.delete("/api/notes/:id", (request, response) => {
-  const myId = request.params.id;
-  notes = notes.filter((note) => note.id !== myId);
-  response.status(202).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  const deletedNote = Note.findByIdAndDelete(id);
+  deletedNote
+    .then(() => {
+      response.status(200).send(`Note deleted successfully with id ${id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-// Adding or creating the new note
+// Updating the existing note
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({
+      error: "Malformed Id",
+    });
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 app.listen(PORT);
