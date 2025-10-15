888dd0b3cc7df69f6fe69d60a4f8313ac78f087c
shiriweb <shirisha.singh36@gmail.com>
2025-10-15 12:14:56 +0545

    feat: initializing test database

--- part3/notes-server/tests/note_api.test.js
+++ part3/notes-server/tests/note_api.test.js
@@ -1,10 +1,29 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const assert = require("node:assert");
-
 const api = supertest(app);
+const Note = require("../model/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
   await api
@@ -16,14 +35,14 @@ test("notes are returned as json", async () => {
 test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
 
-  assert.strictEqual(response.body.length, 29);
+  assert.strictEqual(response.body.length, 2);
 });
 
 test("a specific note is within the returned notes", async () => {
   const response = await api.get("/api/notes");
 
   const contents = response.body.map((e) => e.content);
-  assert.strictEqual(contents.includes("Updated note content"), true);
+  assert.strictEqual(contents.includes("HTML is easy"), true);
 });
 
 after(async () => {
--- part3/notes-server/utils/config.js
+++ part3/notes-server/utils/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const PORT = process.env.PORT ? process.env.PORT : 3001;
 const MONGODB_URI =
-  process.NODE_ENV === "test"
-    ? process.env.TEST_MONGO_URL
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
 module.exports = { MONGODB_URI, PORT };
