e8add9bdfdd8b208aaa73053d9da4e5aa0271c6d
AccountA-Username <accountA@email.com>
2025-12-16 16:00:01 +0545

    feat: completed

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 
-// main 
+// main file
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
