cc753448218b69018b10cc92d18fd26bbded0c6f
Shirisha Singh <shirisha.singh36@gmail.com>
2025-12-19 12:19:43 +0545

    feat: Connecting to postGres from JS

---
+++ part13/notes-server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+
+const express = require('express')
+const app = express()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
+
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  res.json(notes)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
