34fc2408a7da5bbee611e3441a8cbc57c2c7d8e6
Shirisha Singh <shirisha.singh36@gmail.com>
2025-12-22 10:55:41 +0545

    feat: refactoring the code

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -1,86 +1,20 @@
-require('dotenv').config()
+const express = require("express");
+const app = express();
 
-const { Sequelize, Model, DataTypes } = require('sequelize')
-const express = require('express')
-const app = express()
-app.use(express.json())
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
+const notesRouter = require("./controllers/notes");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+app.use(express.json());
 
+app.use("/api/notes", notesRouter);
 
-class Note extends Model {}
-Note.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  important: {
-    type: DataTypes.BOOLEAN
-  },
-  date: {
-    type: DataTypes.DATE
-  },
-   creationYear: {
-    type: DataTypes.INTEGER,
-  },
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'note'
-})
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-Note.sync()
-
-app.get('/api/notes', async (req, res) => {  
-  const notes = await Note.findAll()
-  res.json(notes)
-  console.log(JSON.stringify(notes,null,2));
-
-})
-
-app.post('/api/notes', async (req, res) => {
-  console.log(req.body)
-  const note = await Note.create(req.body)
-  res.json(note)
-})
-
-app.get('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    console.log(note.toJSON());
-    
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.put('/api/notes/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    note.important = req.body.important
-    await note.save()
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start();
